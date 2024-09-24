c01f2d33498f0910c75947952d19a68e9af6daef
locopeligroso <giuntagiuseppe.pro@gmail.com>
2024-09-24 15:58:32 +0200

    basic camera movements

--- src/App.jsx
+++ src/App.jsx
@@ -1,114 +1,145 @@
-import { OrthographicCamera } from '@react-three/drei';
-import { Environment } from '@react-three/drei';
-import { useControls, folder } from 'leva';
-
+import { OrthographicCamera, PerspectiveCamera, SoftShadows } from '@react-three/drei';
+import { useControls } from 'leva';
 import { useState, useEffect, useRef } from 'react';
 import { gsap } from "gsap";
-
-
-import MyGrid from "./utils/MyGrid"
+import MyGrid from "./utils/MyGrid";
 import { cube, plane } from "./utils/geometries.js";
 import { material } from "./utils/materials.js";
 
 export default function App() {
-  const cameraRef = useRef()
-  const target = useRef()
-  const cubeRefOne = useRef()
+  const [isInFocus, setisInFocus] = useState(false);
 
+  const cameraRef = useRef();
+  const perspectiveCameraRef = useRef();
+  const target = useRef();
+  const cubeRefOne = useRef();
+  const cubeRefTwo = useRef();
 
-
-  const { targetX, targetY, targetZ } = useControls("Camera", {
-    cameraZoom: {
-      value: 100,
-      min: 50,
-      max: 1000,
-      step: 1,
-    },
-    targetX: {
-      value: 0,
-      min: -10,
-      max: 10,
-      step: 0.01,
-    },
-    targetY: {
-      value: 0,
-      min: -10,
-      max: 10,
-      step: 0.01,
-    },
-    targetZ: {
-      value: 0,
-      min: -10,
-      max: 10,
-      step: 0.01,
-    }
-  });
+  const { showGrid } = useControls("grid", { showGrid: false });
+  const { toggleCamera } = useControls("camera", { toggleCamera: true });
 
   useEffect(() => {
     if (cameraRef.current && target.current) {
       cameraRef.current.lookAt(target.current.position);
     }
-  }, [targetX, targetY, targetZ]);
+  }, [toggleCamera]);
 
-  function getPosition() {
-    console.log(cubeRefOne.current.position);
-    console.log(cameraRef.current)
+  useEffect(() => {
+    if (perspectiveCameraRef.current) {
+      perspectiveCameraRef.current.lookAt(0, 0, 0);
+    }
+  }, [toggleCamera]);
 
+  function resetCamera() {
     gsap.to(target.current.position, {
-      x: cubeRefOne.current.position.x,
-      y: cubeRefOne.current.position.y,
-      z: cubeRefOne.current.position.z,
+      x: 0,
+      y: 0,
+      z: 0,
       duration: 1
     });
 
     gsap.to(cameraRef.current, {
-      zoom: 200,
-      duration: 3,
+      zoom: 100,
+      duration: 1,
       onUpdate: () => {
-        cameraRef.current.updateProjectionMatrix()
+        cameraRef.current.updateProjectionMatrix();
       }
-    });
 
+    })
+
+    setisInFocus(false);
+  }
+
+  function focusOnCubeOne(ref, zoom) {
+    if (!isInFocus && ref.current) {
+      console.log(ref.current)
+      gsap.to(target.current.position, {
+        x: ref.current.position.x,
+        y: ref.current.position.y,
+        z: ref.current.position.z,
+        duration: 1
+      });
+
+      gsap.to(cameraRef.current, {
+        zoom,
+        duration: 1,
+        onUpdate: () => {
+          cameraRef.current.updateProjectionMatrix();
+        }
+      });
+      setisInFocus(true);
+    } else {
+      resetCamera()
+    }
   }
 
   return (
     <>
-      <group ref={target} position={[targetX, targetY, targetZ]}>
-        <OrthographicCamera
-          ref={cameraRef}
+      {toggleCamera ? (
+        <group ref={target} position={[0, 0, 0]}>
+          <OrthographicCamera
+            ref={cameraRef}
+            makeDefault
+            zoom={100}
+            position={[10, 10, 10]}
+            rotation={[-Math.PI / 6, Math.PI / 6, 0]}
+            near={0.1}
+            far={100}
+          />
+        </group>
+      ) : (
+        <PerspectiveCamera
           makeDefault
-          zoom={100}
+          ref={perspectiveCameraRef}
           position={[10, 10, 10]}
-          rotation={[-Math.PI / 6, Math.PI / 6, 0]}
           near={0.1}
           far={1000}
         />
-      </group>
-      <MyGrid />
-      <Environment preset="city" />
+      )}
+
+      <directionalLight
+        castShadow
+        position={[10, 15, -10]}
+        intensity={4}
+        shadow-mapSize-width={2048}
+        shadow-mapSize-height={2048}
+        shadow-camera-left={-10}
+        shadow-camera-right={10}
+        shadow-camera-top={10}
+        shadow-camera-bottom={-10}
+        shadow-camera-near={0.1}
+        shadow-camera-far={50}
+      />
+      <ambientLight intensity={0.5} />
+      {/* <SoftShadows size={50} focus={0} samples={20} /> */}
+      {showGrid && <MyGrid />}
 
       <mesh
         ref={cubeRefOne}
-        onClick={getPosition}
+        onClick={() => focusOnCubeOne(cubeRefOne, 200)}
+        material={material}
         castShadow
         geometry={cube}
         position={[3, 0.5, 5]}
       />
 
       <mesh
+        ref={cubeRefTwo}
+        onClick={() => focusOnCubeOne(cubeRefTwo, 200)}
         castShadow
         geometry={cube}
         position={[-3, 0.5, -2]}
       />
+
       <mesh
-        castShadow
-        position={[1, -0.001, 0]}
+        receiveShadow
+        position={[1, 0.001, 0]}
         rotation={[-Math.PI * 0.5, 0, 0]}
-        scale={10}
+        scale={50}
         material={material}
         geometry={plane}
       />
     </>
-  );
+  )
 }
 
